449a48348b2e6dc452ac23ba472d104dd43ef09c
Joel Peckham <joelskyler@gmail.com>
2021-09-13 11:37:37 -0400

    Chages

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let testProfile = [[60,20],[65,10],[70,10],[80,10],[100,5],[120,300],[134,300],[150,1500],[145,1000],[100,10],[80,20],[65,30],[60,20]];
 let running = false;
 
 function pulse(rate){
@@ -42,79 +41,119 @@ function getMousePos(canvas, evt) {
     };
 }
 
+class Node{
+    constructor(x,y,r=12){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    draw(canvas, ctx){
+        ctx.beginPath();
+        ctx.fillStyle = "black";
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+class Nodes {
+    constructor(canvas, ctx, controlPoints = 10){
+        this.controlPoints = controlPoints;
+        this.nodes = Array(controlPoints);
+        for (let i = 0; i < controlPoints; i++) {
+            this.nodes[i] = new Node((canvas.width/(controlPoints-1))*i,canvas.height/2);
+        }
+        console.log(this.nodes);
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.dragging = -1;
+        this.draw();
+    }
+    draw(dots = true){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        if (dots){
+            for (const node of this.nodes) {
+                node.draw(this.canvas,this.ctx);
+            }
+        }
+        ctx.moveTo(this.nodes[0].x,this.nodes[0].y);
+        let points = [];
+        for (const node of this.nodes) {
+            points.push(node.x);
+            points.push(node.y);
+        }
+        ctx.curve(points);
+        ctx.stroke();
+        
+    }
+    //returns index of nearest node in nodes array. Retruns -1 if none found.
+    nearestNode(pos){
+        let min = Infinity;
+        let index = -1;
+        for (let i = 0; i < this.nodes.length; i++) {
+            let dist = Math.sqrt(Math.pow(pos.x-this.nodes[i].x,2)+Math.pow(pos.y-this.nodes[i].y,2));
+            if(dist < min){
+                min = dist;
+                index = i;
+            }
+        }
+        return index;
+    }
+    handleMouseDown(pos){
+        let nearest = this.nearestNode(pos);
+        if(nearest != -1){
+            this.dragging = nearest;
+        }
+        this.draw();
+    }
+    handleMouseUp(pos){
+        this.dragging = -1;
+        this.draw();
+    }
+    handleMouseMove(pos){
+        if(this.dragging != -1){
+            this.nodes[this.dragging].y = pos.y;
+        }
+        this.draw();
+    }
+  }
+
 
 const canvas = document.getElementById('profileCanvas');
-canvas.width  = 800; canvas.height = 400;
+let canvasSyle = window.getComputedStyle(canvas);
+canvas.width  = parseInt(canvasSyle.width) * 2;
+canvas.height = parseInt(canvasSyle.height) * 2;
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, 800, 400);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-window.addEventListener("mousemove",(e)=>{mouseMove(e);})
-canvas.addEventListener("mousedown",(e)=>{mouseDown(e);})
+let nodes = new Nodes(canvas,ctx);
 
-let mouseDownInCanvas = false;
-const chop = 10;
-let profile = Array(canvas.width/chop).fill(null);
-function mouseDown(e){
-    // let pos = getMousePos(canvas, e);
-    // let rpos = {x:Math.floor(pos.x / 10)*10,y:Math.floor(pos.y / 10)*10};
-    // profile[rpos.x/10] = rpos.y;
-    // ctx.fillStyle = 'white';
-    // ctx.fillRect(0, 0, 800, 400);  
-    // console.log(profile);
-    // profile.forEach((y,x) => {
-    //     if (y){
-    //         ctx.beginPath();
-    //         ctx.arc(x*10,y,3,0,2*Math.PI,false);
-    //         ctx.fillStyle = "black";
-    //         ctx.fill();
-    //     }
-    // });
-}
+canvas.addEventListener('mousedown', (e)=>{
+    nodes.handleMouseDown(getMousePos(canvas, e));
+});
+window.addEventListener('mouseup', (e)=>{
+    nodes.handleMouseUp(getMousePos(canvas, e));
+});
+canvas.addEventListener('mousemove', (e)=>{
+    nodes.handleMouseMove(getMousePos(canvas, e));
+});
 
-function mouseMove(e){
-    let pos = getMousePos(canvas, e);
-    let changed = false;
-    if(pos.inBounds){
-        let rpos = {x:Math.floor(pos.x / chop)*chop,y:Math.floor(pos.y / chop)*chop};
-        changed = (profile[rpos.x/chop] != rpos.y);
-        if (changed){
-            profile[rpos.x/chop] = rpos.y;
-            ctx.fillStyle = 'white';
-            ctx.fillRect(0, 0, 800, 400);
-            profile.forEach((y,x) => {
-                if (y){
-                    ctx.beginPath();
-                    ctx.arc(x*chop,y,3,0,2*Math.PI,false);
-                    ctx.fillStyle = "black";
-                    ctx.fill();
-                }
-            });
-            ctx.beginPath();
-            ctx.moveTo(0,0);
-            profile.forEach((y,x) => {
-                if (y){
-                    ctx.lineTo(x*chop,y);
-                }
-            });
-            ctx.stroke();
+
+function extractProfileFromCanvas(ctx){
+    let profile = [];
+    let imageData = ctx.getImageData(0,0,ctx.canvas.width,ctx.canvas.height);
+    let data = imageData.data;
+    for (let i = 0; i < ctx.canvas.width; i++) {
+        let column = [];
+        for (let j = 0; j < ctx.canvas.height; j++) {
+            if(data[(j*ctx.canvas.width+i)*4] != 255 || data[(j*ctx.canvas.width+i)*4+1] != 255 || data[(j*ctx.canvas.width+i)*4+2] != 255){
+                column.push(j);
+                break;
+            }
         }
-    }   
+        profile.push(column);
+    }
+    return profile;
 }
-
-
-
-// let profile = {};
-// function draw(e) {
-//     let pos = getMousePos(canvas, e);
-//     profile[pos.x] = pos.y;
-    // ctx.fillStyle = 'white';
-//     ctx.moveTo(0,0);
-//     ctx.fillRect(0, 0, 800, 400);
-//     ctx.beginPath();
-//     for (const c in profile) {
-//         ctx.lineTo(c,profile[c]);
-//       }
-//     ctx.stroke();
-// }
 
